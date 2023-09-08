6c473e4d0759e313e8c0b1582df7c0c7e7f2ebea
chetanyamahana <121246272+chetanyamahana@users.noreply.github.com>
2023-09-08 09:53:39 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,8 @@
+a = int(input("Enter marks of first subject:"))
+b = int(input("Enter marks of second subject:"))
+c = int(input("Enter marks of third subject:"))
+avg = (a+b+c)/3
+if (avg >= 40):
+    print("Pass")
+else:
+    print("Fail")
